21655990043a2a50af97e0c59b7a5f1078aeeea8
Tsygankova Anastasiya <tsygankova303@gmail.com>
2025-01-07 11:04:37 +0700

    Добавлен срок действия jwt

--- app/auth.py
+++ app/auth.py
@@ -1,14 +1,20 @@
-from datetime import datetime, timedelta
-from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
-from jwt import encode, decode
+from jwt import encode
 from jwt.exceptions import ExpiredSignatureError, InvalidTokenError
 from fastapi import Depends
+from datetime import datetime, timedelta, timezone
+
+
 
 SECRET_KEY = "coWNCIONJND834ni942b4u8b484v495h58MIIEpAIBAAKCAQEAwhvqCC+37A+"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 
-def create_jwt_token(data: dict):
-    return encode(data, SECRET_KEY, algorithm=ALGORITHM)
-
+def create_jwt_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    return encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException, Query, Request
+from fastapi import FastAPI, Depends, HTTPException, Query, Request, status
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from .database import engine, get_db
@@ -8,11 +8,12 @@ from .auth import create_jwt_token
 from jwt import decode
 from jwt.exceptions import ExpiredSignatureError, InvalidTokenError
 from typing import Annotated
+from datetime import datetime, timedelta
 
 
-SECRET_KEY = "coWNCIONJND834ni942b4u8b484v495h58"
+SECRET_KEY = "coWNCIONJND834ni942b4u8b484v495h58MIIEpAIBAAKCAQEAwhvqCC+37A+"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+ACCESS_TOKEN_EXPIRE_MINUTES = 3
 
 app = FastAPI()
 
@@ -20,40 +21,37 @@ models.Base.metadata.create_all(bind=engine)
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 
-def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
     try:
         payload = decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        return payload.get("id")
-    except ExpiredSignatureError:
-        pass
+        user_id: int = payload.get("id")
+        if user_id is None:
+            raise credentials_exception
+        return user_id
     except InvalidTokenError:
-        pass
-
-
-def get_current_user_id(request: Request) -> int:
-    user_id = request.session.get("user_id")
-    if not user_id:
-        raise HTTPException(status_code=401, detail="Not authenticated")
-    return user_id
+        raise credentials_exception
 
 
 @app.post("/register/", response_model=schemas.UserResponse)
 def register_user(user: schemas.UserCreate,
                   db: Session = Depends(get_db)):
     existing_user = crud.get_user_by_email(db, user.email)
-    print("1")
     if existing_user:
         raise HTTPException(status_code=400,
                             detail="Email already registered")
     hashed_password = utils.hash_password(user.password)
-    print("2")
     new_user = crud.create_user(db, name=user.name,
                                 email=user.email,
                                 password=hashed_password)
     return new_user
 
 
-@app.post("/login/")  # response_model=schemas.UserResponse
+@app.post("/login/")
 def login(request: Request,
           form_data: OAuth2PasswordRequestForm = Depends(),
           db: Session = Depends(get_db)):
@@ -62,7 +60,9 @@ def login(request: Request,
                                              user.hashed_password):
         raise HTTPException(status_code=401,
                             detail="Incorrect email or password")
-    return {"Authorization": create_jwt_token({"id": user.id}),
+    token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    return {"Authorization": create_jwt_token(data={"id": user.id},
+                                              expires_delta=token_expires),
             "token_type": "Bearer"}
 
 
@@ -75,13 +75,13 @@ def login(request: Request,
 # Возвращает данные юзера, его теги и задачи.
 @app.get("/users/me/", response_model=schemas.UserResponseWithTasks)
 def read_users_me(db: Session = Depends(get_db),
-                  current_user: int = Depends(get_current_user)):
-    user = crud.get_user_by_id(db, current_user)
-    if not current_user:
+                  user_id: int = Depends(get_current_user)):
+    user = crud.get_user_by_id(db, user_id)
+    if not user:
         raise HTTPException(status_code=404,
                             detail="User not found")
-    tags = crud.get_user_goals(db, current_user)
-    tasks = crud.get_user_active_tasks(db, current_user)
+    tags = crud.get_user_goals(db, user_id)
+    tasks = crud.get_user_active_tasks(db, user_id)
     return {
         "id": user.id,
         "name": user.name,
@@ -92,18 +92,18 @@ def read_users_me(db: Session = Depends(get_db),
 
 
 # Редактирование пароля, возвращает юзера.
-# @app.put("/users/me/edit_password/", response_model=schemas.UserResponse)
-# def update_users_password(password_update: schemas.PasswordUpdate,
-#                           db: Session = Depends(get_db),
-#                           current_user: int = Depends(get_user_from_token)):
-#     user = crud.get_user_by_id(db, current_user)
-#     if user is None:
-#         raise HTTPException(status_code=404,
-#                             detail="User not found")
-#     hashed_password = utils.hash_password(password_update.new_password)
-#     user = crud.update_user_password(db=db, user_id=current_user,
-#                                      new_password=hashed_password)
-#     return user
+@app.put("/users/me/edit_password/", response_model=schemas.UserResponse)
+def update_users_password(password_update: schemas.PasswordUpdate,
+                          db: Session = Depends(get_db),
+                          user_id: int = Depends(get_current_user)):
+    user = crud.get_user_by_id(db, user_id)
+    if user is None:
+        raise HTTPException(status_code=404,
+                            detail="User not found")
+    hashed_password = utils.hash_password(password_update.new_password)
+    user = crud.update_user_password(db=db, user_id=user_id,
+                                     new_password=hashed_password)
+    return user
 
 
 # Редактирование юзера, возвращает юзера.
@@ -111,7 +111,7 @@ def read_users_me(db: Session = Depends(get_db),
 def update_users(request: Request,
                  user_update: schemas.UserUpdate,
                  db: Session = Depends(get_db),
-                 user_id: int = Depends(get_current_user_id)):
+                 user_id: int = Depends(get_current_user)):
     user = crud.get_user_by_id(db, user_id)
     if user is None:
         raise HTTPException(status_code=404,
@@ -126,7 +126,7 @@ def update_users(request: Request,
 @app.get("/tags/", response_model=list[schemas.GoalResponse])
 def get_tags(request: Request,
              db: Session = Depends(get_db),
-             user_id: int = Depends(get_current_user_id)):
+             user_id: int = Depends(get_current_user)):
     goals = crud.get_user_goals(db, user_id)
     return goals
 
@@ -136,7 +136,7 @@ def get_tags(request: Request,
 def create_tags(request: Request,
                 tag: schemas.GoalCreate,
                 db: Session = Depends(get_db),
-                user_id: int = Depends(get_current_user_id)):
+                user_id: int = Depends(get_current_user)):
     crud.create_goal(db=db, goal=tag, user_id=user_id)
     tags = crud.get_user_goals(db, user_id)
     return tags
@@ -147,7 +147,7 @@ def create_tags(request: Request,
 def delete_tags(request: Request,
                 tag_id,
                 db: Session = Depends(get_db),
-                user_id: int = Depends(get_current_user_id)):
+                user_id: int = Depends(get_current_user)):
     crud.delete_goal(db=db, goal_id=tag_id)
     tags = crud.get_user_goals(db, user_id)
     return tags
@@ -158,7 +158,7 @@ def delete_tags(request: Request,
 def get_tasks_by_tag(request: Request,
                      tag_id: int,
                      db: Session = Depends(get_db),
-                     user_id: int = Depends(get_current_user_id)):
+                     user_id: int = Depends(get_current_user)):
     tasks = crud.get_user_tasks_by_tag(db, user_id, tag_id)
     return tasks
 
@@ -168,7 +168,7 @@ def get_tasks_by_tag(request: Request,
 def get_tasks(request: Request,
               task: schemas.TaskBase,
               db: Session = Depends(get_db),
-              user_id: int = Depends(get_current_user_id)):
+              user_id: int = Depends(get_current_user)):
     task = crud.create_task(db, task, user_id)
     return task
 
@@ -177,7 +177,7 @@ def get_tasks(request: Request,
 @app.get("/tasks/today/", response_model=list[schemas.TaskResponse])
 def get_today_tasks(request: Request,
                     db: Session = Depends(get_db),
-                    user_id: int = Depends(get_current_user_id)):
+                    user_id: int = Depends(get_current_user)):
     today = date.today()
     tasks = crud.get_user_tasks_by_date(db, user_id, today)
     return tasks
@@ -187,7 +187,7 @@ def get_today_tasks(request: Request,
 @app.get("/tasks/period/", response_model=list[schemas.TaskResponse])
 def get_pediod_tasks(request: Request,
                      db: Session = Depends(get_db),
-                     user_id: int = Depends(get_current_user_id),
+                     user_id: int = Depends(get_current_user),
                      start_day: date = Query(),
                      end_day: date = Query()):
     tasks = crud.get_user_tasks_by_period(db, user_id, start_day, end_day)
@@ -199,7 +199,7 @@ def get_pediod_tasks(request: Request,
 def get_task_by_id(request: Request,
                    task_id: int,
                    db: Session = Depends(get_db),
-                   user_id: int = Depends(get_current_user_id)):
+                   user_id: int = Depends(get_current_user)):
     task = crud.get_user_tasks_by_id(db, task_id)
     if not task:
         raise HTTPException(status_code=404,
@@ -213,7 +213,7 @@ def update_task(request: Request,
                 task_id: int,
                 task_update: schemas.TaskUpdate,
                 db: Session = Depends(get_db),
-                user_id: int = Depends(get_current_user_id)):
+                user_id: int = Depends(get_current_user)):
     task = crud.update_task(db, task_id, task_update)
     return task
 
@@ -223,7 +223,7 @@ def update_task(request: Request,
 def delete_task(request: Request,
                 task_id: int,
                 db: Session = Depends(get_db),
-                user_id: int = Depends(get_current_user_id)):
+                user_id: int = Depends(get_current_user)):
     task = crud.delete_task(db, task_id)
     if task is None:
         raise HTTPException(status_code=404,
